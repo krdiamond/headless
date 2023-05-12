3adef45f773413a2043c4f357dc03e1279e21955
Kristina Diamond <krdiamond@gmail.com>
2023-05-12 15:12:08 -0400

    added graph in

--- script.js
+++ script.js
@@ -8,3 +8,34 @@ if (addToCartButton) {
 }
 
 
+const query = `
+  query {
+    productByHandle(handle: 'the-sky') {
+        title
+        id
+        featuredImage {
+          url
+        }
+      }
+  }
+`;
+
+
+
+const headers = {
+  'Content-Type': 'application/json',
+  'Permissions-Policy': 'interest-cohort=()',
+  'X-Shopify-Storefront-Access-Token': '5d5a8c181ff58af5e3473f6c439d9e4f',
+};
+
+axios.post('https://kd-email-test.myshopify.com/api/2023-01/graphql', { query }, { headers })
+  .then(response => {
+    console.log(response.data);
+    productTitle.textContent = response.data.productByHandle.title;
+  })
+  .catch(error => {
+    console.error(error);
+  });
+
+
+ 
