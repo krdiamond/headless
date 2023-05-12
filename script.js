44921668343e37731cc7e520d1934c6053e9a7c1
Kristina Diamond <krdiamond@gmail.com>
2023-05-12 14:39:14 -0400

    hide graphql request for troubleshooting

--- script.js
+++ script.js
@@ -22,17 +22,17 @@ const query = `
 
 
 
-const headers = {
-  'Content-Type': 'application/json',
-  'Permissions-Policy': 'interest-cohort=()',
-  'X-Shopify-Storefront-Access-Token': '5d5a8c181ff58af5e3473f6c439d9e4f',
-};
+// const headers = {
+//   'Content-Type': 'application/json',
+//   'Permissions-Policy': 'interest-cohort=()',
+//   'X-Shopify-Storefront-Access-Token': '5d5a8c181ff58af5e3473f6c439d9e4f',
+// };
 
-axios.post('https://kd-email-test.myshopify.com/api/2023-01/graphql', { query }, { headers })
-  .then(response => {
-    console.log(response.data);
-    productTitle.textContent = response.data.productByHandle.title;
-  })
-  .catch(error => {
-    console.error(error);
-  });
+// axios.post('https://kd-email-test.myshopify.com/api/2023-01/graphql', { query }, { headers })
+//   .then(response => {
+//     console.log(response.data);
+//     productTitle.textContent = response.data.productByHandle.title;
+//   })
+//   .catch(error => {
+//     console.error(error);
+//   });
