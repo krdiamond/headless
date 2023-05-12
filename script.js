de729c3fd4e3e6061a57fd410da5d275550b811f
Kristina Diamond <krdiamond@gmail.com>
2023-05-12 14:36:22 -0400

    set up graphQL request

--- script.js
+++ script.js
@@ -1,8 +1,37 @@
 const addToCartButton = document.getElementById('addToCart');
+const productTitle = document.getElementById('productTitle');
+
 if (addToCartButton) {
     addToCartButton.addEventListener('click', function() {
         console.log('Button Clicked');
     });
-} else {
-    console.log('Element not found!');
 }
+
+
+const query = `
+  query {
+    productByHandle(handle: 'the-sky') {
+        title
+        id
+        featuredImage {
+          url
+        }
+      }
+  }
+`;
+
+
+
+const headers = {
+  'Content-Type': 'application/json',
+  'X-Shopify-Storefront-Access-Token': '5d5a8c181ff58af5e3473f6c439d9e4f',
+};
+
+axios.post('https://kd-email-test.myshopify.com/api/2023-01/graphql', { query }, { headers })
+  .then(response => {
+    console.log(response.data);
+    productTitle.textContent = response.data.productByHandle.title;
+  })
+  .catch(error => {
+    console.error(error);
+  });
