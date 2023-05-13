111b660fdacaacbcd6b5da183c2afafd930802b7
Kristina Diamond <krdiamond@gmail.com>
2023-05-12 23:24:14 -0400

    local storage is set, products can be added to cart, cart qty is stated

--- script.js
+++ script.js
@@ -1,31 +1,82 @@
+const cartID = localStorage.getItem('cartID');
+const checkOutID = localStorage.getItem('checkOutID');
+
 const addToCartButton = document.getElementById('addToCart');
-const viewCartButton = document.getElementById('viewCart');
-
-
 
 const productTitle = document.getElementById('productTitle');
-const storeUserID = document.getElementById('storeUserID');
-const storeVariantID = document.getElementById('storeVariantID');
 const productImage = document.getElementById('productImage');
+const checkOutLink = document.getElementById('checkOut');
+const cartQty = document.getElementById('cartQty');
 
-if (addToCartButton) {
-    addToCartButton.addEventListener('click', function() {
-        addToCart();
-    });
-}
-
-if (viewCartButton) {
-    viewCartButton.addEventListener('click', function() {
-        viewCart();
-    });
-}
-
+const storeVariantID = document.getElementById('storeVariantID');
 
 const headers = {
   'Accept': 'application/json',
   'Content-Type': 'application/json',
   'X-Shopify-Storefront-Access-Token': '5d5a8c181ff58af5e3473f6c439d9e4f'
 };
+
+// ---------------------------------------------- VIEW CART ---------------------------------------------- //
+const viewCart = () => {
+    const viewCart = `
+    query {
+        cart(id: "${cartID}") {
+            totalQuantity
+        }
+        node(id: "${checkOutID}") {
+            ... on Checkout {
+              id
+              webUrl
+            }
+          }
+      }`;
+
+    axios.post('https://kd-email-test.myshopify.com/api/2023-04/graphql', { query:viewCart }, { headers })
+    .then(response => {
+        console.log("View Cart")
+        console.log(response.data);
+        checkOutLink.setAttribute('href', response.data.data.node.webUrl); 
+        cartQty.textContent =  response.data.data.cart.totalQuantity
+    })
+    .catch(error => {
+        console.error(error);
+    });
+}
+
+// ---------------------------------------------- CREATE CART ---------------------------------------------- //
+
+const createCart = () => {
+    const createCartData = `
+    mutation {
+        cartCreate(input: {}) {
+            cart {
+                id
+                totalQuantity
+            }
+        }
+        checkoutCreate(input: {}) {
+            checkout {
+                id
+                webUrl
+            }
+        }
+    }`;
+
+    axios.post('https://kd-email-test.myshopify.com/api/2023-04/graphql', { query:createCartData }, { headers })
+    .then(response => {
+        console.log('Cart Data');
+        console.log(response.data);
+        localStorage.setItem('cartID', response.data.data.cartCreate.cart.id);
+        localStorage.setItem('checkOutID', response.data.data.checkoutCreate.checkout.id);
+        checkOutLink.setAttribute('href', response.data.data.checkoutCreate.checkout.webUrl); 
+        cartQty.textContent =  response.data.data.cartCreate.cart.totalQuantity;    
+    })
+    .catch(error => {
+        console.error(error);
+    });
+}
+   
+
 
 // ---------------------------------------------- GET PRODUCTS ---------------------------------------------- //
 const getProducts = `
@@ -54,121 +105,49 @@ axios.post('https://kd-email-test.myshopify.com/api/2023-04/graphql', { query:ge
     productTitle.textContent = response.data.data.productByHandle.title;  
     productImage.setAttribute('src', response.data.data.productByHandle.featuredImage.url);
     storeVariantID.setAttribute('variant-id', response.data.data.productByHandle.variants.edges[0].node.id);
-
   })
   .catch(error => {
     console.error(error);
   });
-
-  // ---------------------------------------------- CREATE CART ---------------------------------------------- //
-
-  const createCart = `
-    mutation createCart {
-        checkoutCreate(input: {}) {
-        checkout {
-            id
-        }
-        }
-    }`;
-
-  axios.post('https://kd-email-test.myshopify.com/api/2023-04/graphql', { query:createCart }, { headers })
-  .then(response => {
-    console.log('checkoutID');
-    console.log(response.data);
-    storeUserID.setAttribute('user-id', response.data.data.checkoutCreate.checkout.id);
-  })
-  .catch(error => {
-    console.error(error);
-  });
-
-  // ---------------------------------------------- VIEW CART ---------------------------------------------- //
-
-  const viewCart = () => {
-    const userIDAttribute = storeUserID.getAttribute("user-id");
-    // const splitUserIDAttribute = userIDAttribute.split('/')
-    // const cartIDElement = splitUserIDAttribute[splitUserIDAttribute.length -1 ];
-    // const cartID = "gid://shopify/Cart/" + cartIDElement
-    // console.log(cartID);
-
-    const viewCart = `
-    query viewCart {
-        node(id: "${userIDAttribute}") {
-    ... on Checkout {
-      id
-      lineItems(first: 10) {
-        edges {
-          node {
-            id
-            title
-            quantity
-            variant {
-              id
-              title
-            }
-          }
-        }
-      }
-    }
-  }
-      }`;
-
-    axios.post('https://kd-email-test.myshopify.com/api/2023-04/graphql', { query:viewCart }, { headers })
-    .then(response => {
-        console.log(response.data);
-    })
-    .catch(error => {
-        console.error(error);
-    });
-
-
-  }
-  
-
-  
-
-
-
-
 
 // ---------------------------------------------- ADD ITEM TO CART ---------------------------------------------- //
 
 const addToCart = () => {
-    const userIDAttribute = storeUserID.getAttribute("user-id");
+    const cartID = localStorage.getItem('cartID');
     const variantIDAttribute = storeVariantID.getAttribute("variant-id");
 
     const addItemToCart = `
     mutation {
-        checkoutLineItemsAdd(
-        lineItems: [
-            {
-            variantId: "${variantIDAttribute}",
-            quantity: 1
+        cartLinesAdd(cartId: "${cartID}", lines: {merchandiseId: "${variantIDAttribute}", quantity: 1}) {
+            cart {
+              totalQuantity
             }
-        ],
-        checkoutId: "${userIDAttribute}"
-        ) {
-            checkout {
-                id
-                lineItems(first: 10) {
-                  edges {
-                    node {
-                      title
-                    }
-                  }
-                }
-              }
-        }
+          }
     }`;
 
     axios.post('https://kd-email-test.myshopify.com/api/2023-04/graphql', { query:addItemToCart }, { headers })
     .then(response => {
         console.log('Item Added To Cart');
         console.log(response.data);
-
+        cartQty.textContent =  response.data.data.cartLinesAdd.cart.totalQuantity; 
     })
     .catch(error => {
         console.error(error);
     });
   }
+    
 
+// ---------------------------------------------- INITIALIZE CART ---------------------------------------------- //
 
+if (cartID) {
+    viewCart();
+}
+else {
+    createCart();
+}
+
+if (addToCartButton) {
+    addToCartButton.addEventListener('click', function() {
+        addToCart();
+    });
+}
